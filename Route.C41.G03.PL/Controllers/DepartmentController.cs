0ef961797ac1224d0afb8da7579d7e33a2e939a7
MahmoudKhalefa2020 <mahmoud.khalefa757@gmail.com>
2024-03-21 23:56:23 +0200

    Department-Create

--- Route.C41.G03.PL/Controllers/DepartmentController.cs
+++ Route.C41.G03.PL/Controllers/DepartmentController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Route.C41.G03.BLL.Interfaces;
+using Route.C41.G03.DAL.Models;
 
 namespace Route.C41.G03.PL.Controllers
 {
@@ -15,6 +16,26 @@ namespace Route.C41.G03.PL.Controllers
         {
             var departments = _departmentRepo.GetAll();
             return View(departments);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                var count = _departmentRepo.Add(department);
+                if (count > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+
+            return View(department);
         }
     }
 }
