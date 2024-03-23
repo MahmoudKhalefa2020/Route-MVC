2aea056a8fec90860687ff6959e02f0e1030f758
MahmoudKhalefa2020 <mahmoud.khalefa757@gmail.com>
2024-03-23 14:56:42 +0200

    session 04

---
+++ Route.C41.G03.PL/Controllers/DepartmentController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Route.C41.G03.BLL.Interfaces;
+using Route.C41.G03.DAL.Models;
+
+namespace Route.C41.G03.PL.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private readonly IDepartmentRepository _departmentRepo;
+
+        public DepartmentController(IDepartmentRepository departmentRepo)
+        {
+            _departmentRepo = departmentRepo;
+        }
+        public IActionResult Index()
+        {
+            var departments = _departmentRepo.GetAll();
+            return View(departments);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                var count = _departmentRepo.Add(department);
+                if (count > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+
+            return View(department);
+        }
+
+
+        public IActionResult Details(int? id)
+        {
+            if (!id.HasValue)
+                return BadRequest();
+
+            var department = _departmentRepo.Get(id.Value);
+
+            if (department is null)
+
+                return NotFound();
+
+            return View(department);
+        }
+    }
+}
