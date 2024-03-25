216c68623b46c3a66ec6db60c309c2748706a4cd
MahmoudKhalefa2020 <mahmoud.khalefa757@gmail.com>
2024-03-25 23:39:01 +0200

    Employee Repository

---
+++ Route.C41.G03.BLL/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,14 @@
+﻿using Route.C41.G03.DAL.Models;
+using System.Collections.Generic;
+
+namespace Route.C41.G03.BLL.Interfaces
+{
+    public interface IEmployeeRepository
+    {
+        IEnumerable<Employee> GetAll();
+        Employee GetById(int id);
+        int Add(Employee entity);
+        int Update(Employee entity);
+        int Delete(Employee entity);
+    }
+}
